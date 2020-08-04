ea5cb01f39b09b273a6c1da890b8ee8a2ecf658f
xyf-mac <xu_yanfeng@126.com>
2020-08-04 21:15:54 +0800

    lint

--- assets/code/scene/story/StoryPiece.js
+++ assets/code/scene/story/StoryPiece.js
@@ -1,27 +1,27 @@
-let StoryAudioMgr = require("StoryAudioMgr");
+let StoryAudioMgr = require('StoryAudioMgr');
 
 
 cc.Class({
-    extends: require("Observer"),
+    extends: require('Observer'),
     editor: CC_EDITOR && {
         executeInEditMode: false, // 允许当前组件在编辑器模式下运行。
         playOnFocus: false,
-        menu: "A-StoryMaster/StoryPiece",
+        menu: 'A-StoryMaster/StoryPiece',
 
     },
     properties: {
         _menuNode: null,
         _storyTalk: null,
-        pieceItem: {default: null, visible: false},
+        pieceItem: { default: null, visible: false },
     },
-    _getMsgList() {
+    _getMsgList () {
         return [
             cc.StoryMaster.Msg.PieceShowOptions,
             cc.StoryMaster.Msg.OnPieceTalkOver,
             cc.StoryMaster.Msg.UserTouch,
-        ]
+        ];
     },
-    _onMsg(msg, data) {
+    _onMsg (msg, data) {
         if (msg === cc.StoryMaster.Msg.PieceShowOptions) {
             cc.ObserverMgr.dispatchMsg(cc.StoryMaster.Msg.OnEnableGlobalTouch, false);
             this._menuNode.active = true;
@@ -38,18 +38,18 @@ cc.Class({
             }
         }
     },
-    onLoad() {
+    onLoad () {
         this._initMsg();
         // 检测选项组件
         let children = this.node.children;
         for (let i = 0; i < children.length; i++) {
             let item = children[i];
-            let script = item.getComponent("StoryOptionBox");
+            let script = item.getComponent('StoryOptionBox');
             if (this._menuNode === null && script) {
                 this._menuNode = item;
             }
 
-            let talk = item.getComponent("StoryTalk");
+            let talk = item.getComponent('StoryTalk');
             if (this._storyTalk === null && talk) {
                 this._storyTalk = talk;
             }
@@ -72,7 +72,7 @@ cc.Class({
     },
 
 
-    start() {
+    start () {
         if (this._menuNode) {
             // 派发消息
             cc.ObserverMgr.dispatchMsg(cc.StoryMaster.Msg.PieceHasOptions, null);
