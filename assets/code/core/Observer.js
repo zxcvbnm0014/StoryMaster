8150746fb28f92f70394beb538f13c0f5a2ec999
xyf-mac <xu_yanfeng@126.com>
2020-08-04 21:32:19 +0800

    增加本地消息

--- assets/code/core/Observer.js
+++ assets/code/core/Observer.js
@@ -1,7 +1,7 @@
 let Observer = cc.Class({
     extends: cc.Component,
 
-    _initMsg() {
+    _initMsg () {
         let list = this._getMsgList();
         if (list) {
             for (let k = 0; k < list.length; k++) {
@@ -9,25 +9,37 @@ let Observer = cc.Class({
                 cc.ObserverMgr.addEventListener(msg, this._onMsg, this);
             }
         }
+        let list2 = this._getLocalMsg();
+        for (let key in list2) {
+            this.node.on(key, list2[key]);
+        }
+
     },
 
-    onLoad() {
+    onLoad () {
     },
-    _getMsgList() {
+    _getMsgList () {
         return [];
     },
-    _onMsg(msg, data) {
+    _getLocalMsg () {
+        return {};
+    },
+    _onMsg (msg, data) {
 
     },
 
-    onDisable() {
+    onDisable () {
         // cc.ObserverMgr.removeEventListenerWithObject(this);
     },
-    onEnable() {
+    onEnable () {
         // ObserverMgr.removeEventListenerWithObject(this);
     },
-    onDestroy() {
+    onDestroy () {
         cc.ObserverMgr.removeEventListenerWithObject(this);
+        let list2 = this._getLocalMsg();
+        for (let key in list2) {
+            this.node.off(key, list2[key]);
+        }
     },
 });
 cc.Observer = module.exports = Observer;
