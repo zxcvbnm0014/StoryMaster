2052e0b35363f3ee18d9de779954d2b2aa5cc1e1
xuyanfeng <xu_yanfeng@126.com>
2020-08-04 16:03:19 +0800

    lint-stage

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,26 +1,19 @@
 const micromatch = require('micromatch');
 
 module.exports = {
-    'packages/story-master/panel-test/vue.js': [
-    ],
-    'packages/story-master/**/*.js': [
-        'prettier --write',
-        'eslint --fix',
-        'git add',
-    ],
     'auto/**/*.js': [
         'prettier --write',
         'eslint --fix',
         'git add',
     ],
 
-    // 'packages/story-master/**/*.css': files => {
-    //     const match = micromatch.not(files, ['**/avg-electron/src/lib/**/*.css']);
-    //     if (match.length < 1) {
-    //         return ['git add'];
-    //     }
-    //     return [`stylelint --fix "${match.join(' ')}" `, 'git add'];
-    // },
+    'packages/story-master/**/*.js': files => {
+        const match = micromatch.not(files, ['packages/story-master/panel-test/vue.js']);
+        if (match.length < 1) {
+            return ['git add'];
+        }
+        return [`eslint --fix "${match.join(' ')}" `, 'git add'];
+    },
     'packages/story-master/**/*.css':[
         'stylelint --fix',
         'git add',
